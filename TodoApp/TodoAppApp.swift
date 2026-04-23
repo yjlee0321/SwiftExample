f9519e2783db89689801f689fc1e1409931102db
yjlee0321 <dbswjd574269@naver.com>
2026-04-24 02:09:41 +0900

    TodoApp 예제 생성

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 이윤정 on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
